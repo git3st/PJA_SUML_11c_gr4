631d81abd706149abaaa3a3a6701e4072f6b47c7
git3st <snochowskiwiktor@gmail.com>
2024-06-18 21:23:52 +0200

    App ready

--- src/deployment/app.py
+++ src/deployment/app.py
@@ -1,15 +1,13 @@
+import json
 import os
+import time
+
 import requests
 import streamlit as st
 import pickle
 import pandas as pd
 import argparse
 import logging
-
-# TO DO
-# Test api call
-# Test output
-# Beautify
 
 
 def create_error_logger() -> logging.Logger:
@@ -27,7 +25,7 @@ def create_error_logger() -> logging.Logger:
 logger = create_error_logger()
 
 
-def get_elo(user, api_key):
+def get_elo(user, game_mode, api_key):
     """
     Retrieves the Lichess Elo rating for the given user.
 
@@ -46,7 +44,6 @@ def get_elo(user, api_key):
         Exception: For any other unexpected errors.
     """
 
-    rating = 1500
     try:
         url = f"https://lichess.org/api/user/{user}"
         headers = {
@@ -56,19 +53,34 @@ def get_elo(user, api_key):
         response = requests.get(url=url, headers=headers)
         if response.status_code == 200:
             data = response.json()
-            rating = int(data["profile"]["fideRating"])
+            rating = int(data["perfs"][game_mode]["rating"])
         else:
-            print("Error: {response.status_code}")
+            st.error('Error - we could not communicate with the Lichess backend server (not our fault!)')
+
     except requests.exceptions.RequestException as e:
-        logger.error("Error fetching Lichess data for user '{user}': %s", e)
+        logger.error(f"Error fetching Lichess data for user '{user}': {e}")
+        st.error('Error - we could not communicate with the Lichess backend server (not our fault!)')
     except KeyError as e:
-        logger.error("Elo rating data not found for user '{user}': %s", e)
+        logger.error(f"Elo rating data not found for user '{user}': {e}")
+        st.error('Error - we could not communicate with the Lichess backend server (not our fault!)')
     except ValueError as e:
-        logger.error("Invalid Elo rating format for user '{user}': %s", e)
+        logger.error(f"Invalid Elo rating format for user '{user}': {e}")
+        st.error('Error - we could not communicate with the Lichess backend server (not our fault!)')
     except Exception as e:  # Catch-all for unexpected errors
-        logger.error("Unexpected error fetching Elo for '{user}': %s", e)
+        logger.error(f"Unexpected error fetching Elo for '{user}': {e}")
+        st.error('Error - we could not communicate with the Lichess backend server (not our fault!)')
     return rating
 
+
+def fake_loader(final_result):
+    msg = st.toast('Fetching data for the players...')
+    time.sleep(2)
+    msg.toast('Evaluating game performance...')
+    time.sleep(2)
+    msg.toast('Comparing players...')
+    time.sleep(2)
+    msg.toast('Ready!')
+    st.success(final_result)
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -102,11 +114,13 @@ st.write(
 
 white = st.text_input("White:")
 black = st.text_input("Black:")
+modes = ['bullet', 'blitz', 'rapid', 'classical']
+game_mode = st.selectbox("Game mode:", modes)
 
 
 if st.button("Predict"):
-    white_elo = get_elo(white, api)
-    black_elo = get_elo(black, api)
+    white_elo = get_elo(white, game_mode, api)
+    black_elo = get_elo(black, game_mode, api)
     white_diff = white_elo - black_elo
     black_diff = black_elo - white_elo
     input_data = pd.DataFrame(
@@ -117,8 +131,12 @@ if st.button("Predict"):
             "BlackElo": [black_elo],
         }
     )
+    logger.error(input_data)
 
     prediction = model.predict(input_data)
-
-    st.write(f"Prediction: {prediction[0]} would win!")
+    if prediction[0] == 0:
+        final_result = f'{black} would win with {white} in a {game_mode} game!'
+    elif prediction[0] == 2:
+        final_result = f'{white} would win with {black} in a {game_mode} game!'
+    fake_loader(final_result=final_result)
     st.balloons()
